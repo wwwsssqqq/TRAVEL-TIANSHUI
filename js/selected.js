61c26226516a2ec3f5093f44883fbfe0e5d6f5ac
wsq <2876110865@qq.com>
2017-04-09 12:52:41 +0800

    add js

---
+++ js/selected.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    // 加入收藏
+    $(".J-selected").click(function(){
+              alert("请按下“Ctrl+D”进行收藏！");
+    });
+
+    // banner轮播图
+    $(".J-slide").animate(function() {
+          // var slideArray = $("#J-slide").getElementByTagName(a);
+          
+        
+
+
+    });
+  });
+
